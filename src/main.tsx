d3d258fd5921fb65dd430c586026894bc93b5956
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2025-04-09 18:44:43 -0600

    feat: route

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App.tsx'
 import { StrictMode } from 'react'
+import { RouterProvider } from 'react-router'
+import { router } from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode> 
 )
