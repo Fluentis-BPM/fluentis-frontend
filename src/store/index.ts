580d3f787f0392108081cdd5e842a9bad3b2bb47
Diego Patzan <diegopatzan24@gmail.com>
2025-08-12 13:37:49 -0600

    feature/fluen254_GruposAprobacion

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
+import approvalGroupsReducer from './approvalGroups/approvalGroupsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    approvalGroups: approvalGroupsReducer,
   },
 });
 
